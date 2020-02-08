a6124061d3cbc68b0c19aaf0aaa9c53c062dc7a7
José Bioni da Fonseca Filho <1196702230@acd.grupotiradentes.com>
2020-02-07 21:04:41 -0300

    adição classe soma

---
+++ src/programacao03/Soma.java
@@ -0,0 +1,12 @@
+package programacao03;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 11;
+		
+System.out.println("A soma dos números dá: " + (a + b));
+	}
+
+}
